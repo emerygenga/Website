5b4224d572d32d702a2c7502c8ef8f24fc6d6621
emerygenga <70218311+emerygenga@users.noreply.github.com>
2020-11-18 12:05:37 -0800

    Game Button

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function loadContent(content){
   $("#game_content").html (content);
 }
 function loadGame(){
-  $("game_content").html("");
+  $("#game_content").html("");
   $.ajax({url: "game.html", success: loadContent});
 }
 $("#game_button").click(loadGame);
