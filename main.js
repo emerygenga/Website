e476379c27297cf6dee380ffc93cfd7dd68f2868
emerygenga <70218311+emerygenga@users.noreply.github.com>
2020-12-01 21:02:35 -0800

    new changes

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ function loadGame(){
   $.ajax({url: "game.html", success: loadContent});
 }
 $("#game_button").click(loadGame);
+
+function loadJSONAsHTML(result){
+  var mySting = "";
+  for (var i = 0; i < result.length; i++)
+  {
+    var nextResult = result [i];
+    myString += "<p>" + nextResult[i] + "</p>";
+  }
+}
+
+function requestData(){
+  $.ajax({
+    dataType: "json",
+    url: "https://firebasestorage.googleapis.com/v0/b/advancedtopicscs.appspot.com/o/blogArticles.json?alt=media&token=478d06a6-d7e5-4cac-a92d-7670dea8de17",
+    success: loadJSONAsHTML
+  })
+}
+
+function loadJson(contnt){
+  $("#JSON_button").html (content);
+}
+
+$("JSON_button").click(loadJSONAsHTML);
